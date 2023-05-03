b97d01c72a1446ac2b51ad1ac0f839b65743ec95
thedevyash <imailsharma.yash@gmail.com>
2023-05-04 00:31:57 +0530

    mvc

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mic_music_app/main.dart';
+import 'package:music_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MICMusicApp());
+    await tester.pumpWidget(const Myapp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
